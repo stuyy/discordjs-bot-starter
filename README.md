785b3f0b7cea72b41e2521bbe94a00a91527e29f
ansonfoong <ansfoong@gmail.com>
2020-08-02 09:31:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Instructions
+
+To use this code, follow the instructions:
+
+1) Install all dependencies:
+
+    `npm i`
+
+2) Create a .env file for environment variables in the root directory of this repository, not inside the `src` folder!
+
+3) Create three environment variables:
+    - **DISCORDJS_BOT_TOKEN** - Your Bot Token
+    - **WEBHOOK_ID** - For webhooks, not required unless you want to use the webhook command
+    - **WEBHOOK_TOKEN** - The token for your webhook client, refer to 1:12:00 in the video on how to create a Webhook.
+
+4) Run `npm run start` or `npm run dev` in the project directory
+
+# Notes
+
+- Keep in mind all of the role IDs, channel IDs, etc. were all for my test server that I used for the tutorial. You must configure it yourself by changing the IDs to meet your needs.
---
+++ src/bot.js
@@ -0,0 +1,103 @@
+require("dotenv").config();
+
+const { Client, WebhookClient } = require('discord.js');
+
+const client = new Client({
+  partials: ['MESSAGE', 'REACTION']
+});
+
+const webhookClient = new WebhookClient(
+  process.env.WEBHOOK_ID,
+  process.env.WEBHOOK_TOKEN,
+);
+
+const PREFIX = "$";
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on('message', async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(PREFIX)) {
+    const [CMD_NAME, ...args] = message.content
+      .trim()
+      .substring(PREFIX.length)
+      .split(/\s+/);
+    if (CMD_NAME === 'kick') {
+      if (!message.member.hasPermission('KICK_MEMBERS'))
+        return message.reply('You do not have permissions to use that command');
+      if (args.length === 0)
+        return message.reply('Please provide an ID');
+      const member = message.guild.members.cache.get(args[0]);
+      if (member) {
+        member
+          .kick()
+          .then((member) => message.channel.send(`${member} was kicked.`))
+          .catch((err) => message.channel.send('I cannot kick that user :('));
+      } else {
+        message.channel.send('That member was not found');
+      }
+    } else if (CMD_NAME === 'ban') {
+      if (!message.member.hasPermission('BAN_MEMBERS'))
+        return message.reply("You do not have permissions to use that command");
+      if (args.length === 0) return message.reply("Please provide an ID");
+      try {
+        const user = await message.guild.members.ban(args[0]);
+        message.channel.send('User was banned successfully');
+      } catch (err) {
+        console.log(err);
+        message.channel.send('An error occured. Either I do not have permissions or the user was not found');
+      }
+    } else if (CMD_NAME === 'announce') {
+      console.log(args);
+      const msg = args.join(' ');
+      console.log(msg);
+      webhookClient.send(msg);
+    }
+  }
+});
+
+client.on('messageReactionAdd', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '738666523408990258') {
+    switch (name) {
+      case '🍎':
+        member.roles.add('738664659103776818');
+        break;
+      case '🍌':
+        member.roles.add('738664632838782998');
+        break;
+      case '🍇':
+        member.roles.add('738664618511171634');
+        break;
+      case '🍑':
+        member.roles.add('738664590178779167');
+        break;
+    }
+  }
+});
+
+client.on('messageReactionRemove', (reaction, user) => {
+  const { name } = reaction.emoji;
+  const member = reaction.message.guild.members.cache.get(user.id);
+  if (reaction.message.id === '738666523408990258') {
+    switch (name) {
+      case '🍎':
+        member.roles.remove('738664659103776818');
+        break;
+      case '🍌':
+        member.roles.remove('738664632838782998');
+        break;
+      case '🍇':
+        member.roles.remove('738664618511171634');
+        break;
+      case '🍑':
+        member.roles.remove('738664590178779167');
+        break;
+    }
+  }
+});
+
+client.login(process.env.DISCORDJS_BOT_TOKEN);
